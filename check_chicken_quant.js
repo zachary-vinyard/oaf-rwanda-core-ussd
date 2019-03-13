48945a5e0df16d0b292419e2b690b678dff71c6b
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-03-13 16:55:56 +0200

    updated chickens

---
+++ check_chicken_quant.js
@@ -0,0 +1,134 @@
+//must set state.vars.max_chickens
+//must set $client_name
+//can return state.vars.chicken_confirm
+
+var client_dat = JSON.parse(state.vars.client_json);
+$client_name = client_dat.ClientName;
+//sendEmail('zachary.vinyard@oneacrefund.org','test',state.vars.client_json)
+console.log('name : ' + $client_name + ' an : ' + state.vars.account_number + '\n' + client_dat);
+//console.log(client_dat.BalanceHistory[0].Balance)
+var chicken_clients = project.getOrCreateDataTable('chicken_clients');
+var client = chicken_clients.queryRows({vars: {'account_number' : state.vars.account_number}}).next();
+
+var month = moment().format("MMMM");
+console.log(month);
+var calc_max_chicks = {
+    'December' : function(client){
+        var arrayLength = client_dat.BalanceHistory.length;
+        $balance = '';
+        $paid = 0;
+        $credit = 0;
+        for (var i = 0; i < arrayLength; i++) {
+            if(client_dat.BalanceHistory[i].Balance > 0){
+                $paid = client_dat.BalanceHistory[i].TotalCredit-client_dat.BalanceHistory[i].Balance;
+                $balance = client_dat.BalanceHistory[i].Balance;
+                $credit = client_dat.BalanceHistory[i].TotalCredit;
+        
+            }
+        }
+        if(!$paid){$paid = 0}
+        if(!$credit){$credit = 0}
+        if ($balance === ''){$balance = 0}
+        $check_math =Math.min(Math.floor((Math.max($paid- $credit*0.6)/500)));
+        $result = Math.max(Math.min(client.vars.ordered_chickens, $check_math),0);
+        console.log(client.vars.ordered_chickens);
+        return $result;
+    },
+    'January' : function(client){
+        var arrayLength = client_dat.BalanceHistory.length;
+        
+        $balance = '';
+        $paid = 0;
+        $credit = 0;
+        for (var i = 0; i < arrayLength; i++) {
+            if(client_dat.BalanceHistory[i].Balance > 0){
+                $paid = client_dat.BalanceHistory[i].TotalCredit-client_dat.BalanceHistory[i].Balance;
+                $balance = client_dat.BalanceHistory[i].Balance;
+                $credit = client_dat.BalanceHistory[i].TotalCredit;
+        
+            }
+        }
+        if(!$paid){$paid = 0}
+        if(!$credit){$credit = 0}
+        if ($balance === ''){$balance = 0}
+        $check_math = Math.max(Math.floor(($paid - client.vars.a_credit*0.6)/500),0);
+        //old don't use
+        //$check_math =Math.min(Math.floor((Math.max($paid- $credit*0.6)/500)));
+        //$result = Math.max(Math.min(client.vars.ordered_chickens, $check_math),0);
+        $result = Math.min(client.vars.ordered_chickens, $check_math);
+        console.log(client.vars.ordered_chickens +  ' ' + $result);
+        console.log($paid);
+        console.log($balance)
+        console.log($check_math)
+        return $result;
+    },
+    'February' : function(client){
+        var arrayLength = client_dat.BalanceHistory.length;
+        
+        $balance = '';
+        $paid = 0;
+        $credit = 0;
+        for (var i = 0; i < arrayLength; i++) {
+            if(client_dat.BalanceHistory[i].Balance > 0){
+                $paid = client_dat.BalanceHistory[i].TotalCredit-client_dat.BalanceHistory[i].Balance;
+                $balance = client_dat.BalanceHistory[i].Balance;
+                $credit = client_dat.BalanceHistory[i].TotalCredit;
+        
+            }
+        }
+        if(!$paid){$paid = 0}
+        if(!$credit){$credit = 0}
+        if ($balance === ''){$balance = 0}
+        $check_math = Math.max(Math.floor(($paid - client.vars.a_credit*0.6)/500),0);
+        //old don't use
+        //$check_math =Math.min(Math.floor((Math.max($paid- $credit*0.6)/500)));
+        //$result = Math.max(Math.min(client.vars.ordered_chickens, $check_math),0);
+        $result = Math.min(client.vars.ordered_chickens, $check_math);
+        console.log(client.vars.ordered_chickens +  ' ' + $result);
+        console.log($paid);
+        console.log($balance)
+        console.log($check_math)
+        return $result;
+    },
+    'March' : function(client){
+        var arrayLength = client_dat.BalanceHistory.length;
+        
+        $balance = '';
+        $paid = 0;
+        $credit = 0;
+        $overpayment = 0;
+        for (var i = 0; i < arrayLength; i++) {
+            console.log(client_dat.BalanceHistory[i].Balance);
+            if(client_dat.BalanceHistory[i].Balance > 0){
+                $paid = client_dat.BalanceHistory[i].TotalCredit-client_dat.BalanceHistory[i].Balance;
+                $balance = client_dat.BalanceHistory[i].Balance;
+                $credit = client_dat.BalanceHistory[i].TotalCredit;
+            }
+            if(client_dat.BalanceHistory[i].Balance < 0){
+                $overpayment += client_dat.BalanceHistory[i].Balance;
+            }
+        }
+        console.log($overpayment)
+        if(!$paid){$paid = 0}
+        if(!$credit){$credit = 0}
+        if ($balance === ''){$balance = 0}
+        if($paid ===0 && $overpayment !== 0){$paid = (-1)*$overpayment}
+        $check_math = Math.max(Math.floor(($paid - client.vars.a_credit*0.6)/500),0);
+        $result = Math.min(client.vars.ordered_chickens, $check_math);
+        console.log(client.vars.ordered_chickens +  ' ' + $result);
+        console.log("paid" + $paid)
+        console.log($check_math)
+        return $result;
+    },
+    'placeholder' : function(client){
+        return client.vars.ordered_chickens | 0;
+    }
+};
+//this is the placeholder bit now - change placeholder to month activate
+state.vars.max_chickens = calc_max_chicks[month](client);
+if(client.vars.confirmed === 1){
+    state.vars.confirmed_chickens = client.vars.confirmed_chickens;
+    //state.vars.chicken_confirm = client.vars.confirmed_chickens;
+    state.vars.confirmation_number = client.vars.confirmation_number;
+    $confirmed = 1;
+}
