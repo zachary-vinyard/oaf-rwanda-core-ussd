c5606c9a7263feb42581b097ef0ea796b453074b
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-03-13 14:40:46 +0200

    initial chicken agent commit

---
+++ chicken_get_agent.js
@@ -0,0 +1,9 @@
+var chick_loc_info = project.getOrCreateDataTable('chick_loc_info');
+var cursor = chick_loc_info.queryRows({vars : {'site' : state.vars.client_site}});
+if(!cursor.hasNext()){
+    sendEmail(admin_email, 'Chicken problems','chicken get agent row 2');
+}
+var loc = cursor.next();
+$agent_loc = loc.vars.agent_loc;
+$agent_cell = loc.vars.agent_cell;
+$agent_village = loc.vars.agent_village;
