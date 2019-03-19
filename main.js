2518285dc1d948562c25a7bbbc12b16a27562fec
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-03-19 10:32:34 +0200

    bugfix

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ lang options - 'en', 'ki'
 var msgs = require('./lib/msg-retrieve'); //global message handler
 
 global.main = function () {
-    sayText(msgs.get_message(LANG,'main_splash'));
+    sayText(msgs.get_message('main_splash'));
     promptDigits('account_number_splash', { 'submitOnHash' : false,
                                             'maxDigits'   : 8,
                                             'timeout'     : 180 });
