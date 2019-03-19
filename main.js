bac435cb1757dd3c16060a5a9ccdb149ef7fff9e
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-03-19 13:04:30 +0200

    updates account-verify and main

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ include more documentation here!!
 lang options - 'en', 'ki'
 */
 
-var msgs = require('./lib/msg-retrieve'); //global message handler
+var msgs = require('./lib/msg-retrieve').get_message; //global message handler
+var admin_alert = require('.lib/admin-alert').admin_alert; //global admin alerter
 
 global.main = function () {
     sayText(msgs.get_message('main_splash'));
@@ -18,7 +19,21 @@ global.main = function () {
 input handlers - one per response variable
 */
 
-addInputHandler('account_number_splash',function(){ //acount_number_splash input handler - main input handler for initial splash
-    sayText(msgs.get_message('account_number_verified'));
-    stopRules();
+addInputHandler('account_number_splash',function(input){ //acount_number_splash input handler - main input handler for initial splash
+    try{
+        var response = input.replace(/\D/g,'')
+        var client_verified = require('./account-verify').account_verify(response)
+        if(client_verified){
+            sayText(msgs('account_number_verified'));
+            stopRules();
+        }
+        else{
+            sayText(msgs('account_number_not_found'));
+            stopRules();
+        }
+    }
+    catch(error){
+        admin_alert("ERROR, ERROR, ERROR", 'Error on USSD test integration')
+        stopRules();
+    }
 });
