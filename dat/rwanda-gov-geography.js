edc46ab00b6ff8079932e1a625d47f73d4137314
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-03-19 17:27:13 +0200

    updated geo data

---
+++ dat/rwanda-gov-geography.js
@@ -0,0 +1,46 @@
+/*
+module contains a file with government geography for Rwanda
+*/
+module.exports = {"Rwanda" : {
+    "Eastern"   : {
+        "Bugesera"   : {},
+        "Gatsibo"    : {},
+        "Kayonza"    : {},
+        "Kirehe"     : {},
+        "Ngoma"      : {},
+        "Nyagatare"  : {},
+        "Rwamagana"  : {}
+    },
+    "Western"   : {
+        "Karongi"    : {},
+        "Ngororero"  : {},
+        "Nyabihu"    : {},
+        "Nyamasheke" : {},
+        "Rubavu"     : {},
+        "Rusizi"     : {},
+        "Rutsiro"    : {},
+        "Mukamira"   : {},
+    },
+    "Southern"  : {
+        "Gisagara"   : {},
+        "Huye"       : {},
+        "Kamonyi"    : {},
+        "Muhanga"    : {},
+        "Nyamagabe"  : {},
+        "Nyanza"     : {},
+        "Nyaruguru"  : {},
+        "Ruhango"    : {}
+    },
+    "Northern"  : {
+        "Burera"     : {},
+        "Gakenke"    : {},
+        "Gicumbi"    : {},
+        "Musanze"    : {},
+        "Rulindo"    : {}
+    },
+    "Kigali" : {
+        "Gasabo"     : {},
+        "Kicukiro"   : {},
+        "Nyarugenge" : {}
+    }
+}};
