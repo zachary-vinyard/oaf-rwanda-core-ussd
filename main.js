f46ef4119d2aef2f78bdd3a5a193fe08a6a6f030
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-03-19 10:06:13 +0200

    main updates

--- main.js
+++ main.js
@@ -4,8 +4,22 @@ include more documentation here!!
 lang options - 'en', 'ki'
 */
 
+var LANG = 'en'; //en or ki - change to ki for launch
+var msgs = require('./lib/msg-retrieve'); //global message handler
+
 global.main = function () {
-    var LANG = 'en'; //en or ki
-    var msgs = require('./lib/msg-retrieve');
     sayText(msgs.get_message(LANG,'main_splash'));
+    promptDigits('account_number_splash', { 'submitOnHash' : false,
+                                            'maxDigits'   : 8,
+                                            'timeout'     : 180 });
 };
+
+
+/*
+input handlers - one per response variable
+*/
+
+addInputHandler('account_number_splash',function(){ //acount_number_splash input handler - main input handler for initial splash
+    sayText(msgs.get_message(LANG,'account_number_verified'));
+    stopRules();
+});
