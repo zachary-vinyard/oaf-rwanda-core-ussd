098b93b8ea067b2ca73ff535479ca657a35f960f
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-04-01 12:22:43 +0200

    enr 0.01 initial commit

---
+++ enrollment.js
@@ -0,0 +1,22 @@
+/*
+main module for all enrollment functions in OAF Rwanda
+including core, expansion, and Ruhango
+*/
+
+var msgs = require('./lib/msg-retrieve');
+var admin_alert = require('./lib/admin-alert');
+var populate_menu = require('./lib/populate-menu')
+var lang = project.getOrCreateDataTable('ussd_settings').queryRows({'vars' : {'settings' : 'enr_lang'}}).next().vars.value;
+
+global.main = function(){
+    sayText(msgs('enr_splash', {}, lang))
+    promptDigits('enr_splash', {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
+}
+
+addInputHandler('enr_splash', function(input){ //input
+    state.vars.current_step = 'enr_splash';
+    input = parseInt(input.replace(/\D/g,''));
+    var splash_menu = populate_menu('enr_splash', lang);
+    console.log(splash_menu);
+});
+
