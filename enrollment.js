496db10998ee3269abe9076b45fbcdc1c43a12c7
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-04-01 12:50:27 +0200

    enr 0.0.2 dubois sea snake

--- enrollment.js
+++ enrollment.js
@@ -9,14 +9,15 @@ var populate_menu = require('./lib/populate-menu')
 var lang = project.getOrCreateDataTable('ussd_settings').queryRows({'vars' : {'settings' : 'enr_lang'}}).next().vars.value;
 
 global.main = function(){
-    sayText(msgs('enr_splash', {}, lang))
+    var splash_menu = populate_menu('enr_splash', lang);
+    console.log(splash_menu);
+    sayText(msgs('enr_splash', {'$ENR_SPLASH' : splash_menu}, lang))
     promptDigits('enr_splash', {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
 }
 
 addInputHandler('enr_splash', function(input){ //input
     state.vars.current_step = 'enr_splash';
     input = parseInt(input.replace(/\D/g,''));
-    var splash_menu = populate_menu('enr_splash', lang);
-    console.log(splash_menu);
+    
 });
 
