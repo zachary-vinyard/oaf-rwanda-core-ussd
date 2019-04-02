452c54434b4d9dc9e15fb59c1c0a59f72b337a97
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-04-02 16:31:11 +0200

    enr 0.0.18 cape cobra

--- enrollment.js
+++ enrollment.js
@@ -10,7 +10,7 @@ var populate_menu = require('./lib/populate-menu')
 var get_menu_option = require('./lib/get-menu-option');
 
 /*
-global options - feel free to refactor someday
+global options - feel free to refactor someday future friends
 */
 var lang = project.getOrCreateDataTable('ussd_settings').queryRows({'vars' : {'settings' : 'enr_lang'}}).next().vars.value;
 
@@ -26,7 +26,7 @@ global.main = function(){
     promptDigits('enr_splash', {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
 }
 
-addInputHandler('enr_splash', function(input){ //input
+addInputHandler('enr_splash', function(input){ //input handler for splash - expected inputs in table 'enr_splash' on tr
     state.vars.current_step = 'enr_splash';
     input = parseInt(input.replace(/\D/g,''));
     var selection = get_menu_option(input, state.vars.current_step);
@@ -42,10 +42,59 @@ addInputHandler('enr_splash', function(input){ //input
     }
 });
 
-addInputHandler('enr_reg', function(input){
+
+/*
+splash menu function 1-5
+*/
+addInputHandler('enr_reg', function(input){ //needs to be updated
     state.vars.current_step = 'enr_reg';
 });
 
+addInputHandler('enr_order_an', function(input){ //needs to be updated
+    state.vars.current_step = 'enr_order_an';
+});
+
+addInputHandler('enr_order_review_an', function(input){ //needs to be updated
+    state.vars.current_step = 'enr_order_review_an';
+});
+
+addInputHandler('enr_finalize_an', function(input){ //needs to be updated
+    state.vars.current_step = 'enr_finalize_an';
+});
+
+addInputHandler('enr_glus_id', function(input){ //needs to be updated
+    state.vars.current_step = 'enr_glus_id';
+});
+
+
+/*
+input handlers for registration steps
+TODO: complete these!
+*/
+addInputHandler('enr_nid_confirm', function(input){ //step for dd of nid. input here should match stored nid
+    state.vars.current_step = 'enr_nid_confirm';
+});
+
+addInputHandler('enr_name_1', function(input){ //enr name 1 step
+    state.vars.current_step = 'enr_name_1';
+});
+
+addInputHandler('enr_name_1', function(input){ //enr name 2 step
+    state.vars.current_step = 'enr_name_1';
+});
+
+addInputHandler('enr_pn', function(input){ //enr phone number step
+    state.vars.current_step = 'enr_pn';
+});
+
+addInputHandler('enr_glus', function(input){ //enr group leader / umudugudu support id step. should be last registration step
+    state.vars.current_step = 'enr_glus';
+});
+//end registration steps input handlers
+
+/*
+input handler for invalid input - most input handlers dump here for unrecognized input
+*/
 addInputHandler('invalid_input', function(input){
     input = parseInt(input.replace(/\D/g,''));
     if(input == 1){
@@ -53,7 +102,7 @@ addInputHandler('invalid_input', function(input){
         promptDigits(state.vars.current_step, {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
     }
     else if(input == 99){
-        sayText(msgs('exit',{},lang));
+        sayText(msgs('exit', {}, lang));
         stopRules();
     }
 });
