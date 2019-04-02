d28b01ca8b7e0fd370d3edc3c9b879dbf505c249
zachary-vinyard <zachary.vinyard@oneacrefund.org>
2019-04-02 15:33:54 +0200

    enr 0.0.17 russell's viper

--- enrollment.js
+++ enrollment.js
@@ -1,6 +1,7 @@
 /*
 main module for all enrollment functions in OAF Rwanda
 including core, expansion, and Ruhango
+input handler IDs must match the associated menu tables. if input handlers are not available, functions will fail
 */
 
 var msgs = require('./lib/msg-retrieve');
@@ -19,7 +20,9 @@ main function
 global.main = function(){
     console.log(lang);
     var splash_menu = populate_menu('enr_splash', lang);
-    sayText(msgs('enr_splash', {'$ENR_SPLASH' : splash_menu}, lang))
+    var current_menu = msgs('enr_splash', {'$ENR_SPLASH' : splash_menu}, lang);
+    state.vars.current_menu_str = current_menu;
+    sayText(current_menu);
     promptDigits('enr_splash', {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
 }
 
@@ -27,8 +30,30 @@ addInputHandler('enr_splash', function(input){ //input
     state.vars.current_step = 'enr_splash';
     input = parseInt(input.replace(/\D/g,''));
     var selection = get_menu_option(input, state.vars.current_step);
-    console.log(selection);
-    console.log(lang);
-    sayText(msgs(selection, {}, lang));
-    promptDigits(selection, {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180})
+    if(selection == null){
+        sayText(msgs('invalid_input', {}, lang));
+        promptDigits('invalid_input', {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
+    }
+    else{
+        var current_menu = msgs(selection, {}, lang);
+        state.vars.current_menu_str = current_menu;
+        sayText(current_menu);
+        promptDigits(selection, {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180})
+    }
+});
+
+addInputHandler('enr_reg', function(input){
+    state.vars.current_step = 'enr_reg';
+});
+
+addInputHandler('invalid_input', function(input){
+    input = parseInt(input.replace(/\D/g,''));
+    if(input == 1){
+        sayText(state.vars.current_menu_str);
+        promptDigits(state.vars.current_step, {'submitOnHash' : false, 'maxDigits' : 1,'timeout' : 180});
+    }
+    else if(input == 99){
+        sayText(msgs('exit',{},lang));
+        stopRules();
+    }
 });
