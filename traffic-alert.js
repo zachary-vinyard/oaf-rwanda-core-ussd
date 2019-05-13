68693bc96a6bba85fac967157b7244ac6aa9a307
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2019-05-12 17:48:15 -0700

    2.1.0 added traffic alert

---
+++ traffic-alert.js
@@ -0,0 +1,21 @@
+/*
+recurring traffic alert for telerivet
+*/
+
+global.main = function(){
+    cursor = project.queryMessages({message_type: "call", sort_dir: "desc"});
+    cursor.limit(1);
+    var Row = cursor.next();
+    var now = moment().format('X');
+    dif_min = (now - Row.time_updated)/60;
+    console.log("Last message reveived in " + dif_min +" minutes");
+    if (dif_min > 15){
+        var subject = "ALERT: No traffic on USSD service the last 15 min";
+        var body = "No traffic on USSD service the last 15 min please check https://telerivet.com/p/8799a79f/messages for more information";
+        var admin_alert = require('./lib/admin-alert');
+        admin_alert(body, subject);
+        admin_alert(body, subject, 'norbert');
+        admin_alert(body, subject, 'tom');
+        admin_alert(body, subject, 'ammar');
+    }
+};
