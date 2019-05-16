28358db10b98cecd15a2f9e66ad57079ae095aae
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2019-05-16 08:13:22 -0700

    2.2.10 Merriam's kangaroo rat

--- enrollment.js
+++ enrollment.js
@@ -221,6 +221,7 @@ addInputHandler('enr_order_start', function(input){ //input is account number
         promptDigits('enr_continue', {'submitOnHash' : false, 'maxDigits' : max_digits_for_input, 'timeout' : timeout_length});
     }
     else if(client.vars.registered == 1){
+        console.log(client.vars.glus);
         var ruhango_trial_glus_list = ['VA777763', 'VA715591', 'VA933385', 'VA790067', 'VA345963', 'VA548975', 'VA274555', 'VA440341', 'VA307683', 'VA178482'];
         if(client.vars.glus in ruhango_trial_glus_list){
             var check_prep = require('./lib/enr-rgo-check-prep');
