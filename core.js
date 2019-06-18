6df11beaa82126361c0cd9fda08c49164b8cae4b
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2019-06-18 13:17:12 +0200

    2.2.32 Western Saharan spiny mouse

--- core.js
+++ core.js
@@ -7,14 +7,15 @@ var msgs = require('./lib/msg-retrieve'); //global message handler
 var admin_alert = require('./lib/admin-alert'); //global admin alerter
 
 //options
+var settings_table = project.getOrCreateDataTable('ussd_settings');
 const max_digits_for_input = parseInt(settings_table.queryRows({'vars' : {'settings' : 'max_digits'}}).next().vars.value); //only for testing
 const max_digits_for_nid = parseInt(settings_table.queryRows({'vars' : {'settings' : 'max_digits_nid'}}).next().vars.value); 
-const max_digits_for_account_number = parsInt(settings_table.queryRows({'vars' : {'settings' : 'max_digits_nid'}}).next().vars.value);
+const max_digits_for_account_number = parsInt(settings_table.queryRows({'vars' : {'settings' : 'max_digits_an'}}).next().vars.value);
 
 global.main = function () {
     sayText(msgs('cor_main_splash'));
     promptDigits('account_number_splash', { 'submitOnHash' : false,
-                                            'maxDigits'    : 8,
+                                            'maxDigits'    : max_digits_for_account_number,
                                             'timeout'      : 180 });
 };
 
