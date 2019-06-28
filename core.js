2a61456ed9029178a7c7799dd3a3726c41599039
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2019-06-28 15:15:50 +0200

    2.2.56 Thomas's Ethiopian brush-furred rat

--- core.js
+++ core.js
@@ -79,7 +79,6 @@ addInputHandler('cor_menu_select', function(input){
             console.log('got here');
             var get_available_chx = require('./lib/chx-calc-available-chickens');
             var opts = get_available_chx(state.vars.account_number, JSON.parse(state.vars.client_json), chicken_client_table);
-            console.log(JSON.parse(opts));
             state.vars.max_chx = opts.$CHX_NUM;
         }
         else{
