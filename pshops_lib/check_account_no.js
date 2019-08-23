32347341e1679f90fd7c2595adf81b84766761b6
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2019-08-23 12:16:03 +0200

    0.0.28 gull island vole

--- pshops_lib/check_account_no.js
+++ pshops_lib/check_account_no.js
@@ -5,10 +5,18 @@
 */
 
 module.exports = function(accnum){
+    // some code to be able to use Roster API
+    var client = null;
+    var api = require('ext/Roster_v1_1_0/api'); //roster. todo make more general
+    api.dataTableAttach('ExternalApis');
+    var country = project.getOrCreateDataTable('ussd_settings').queryRows({'vars' : {'settings' : 'country'}}).next().vars.value; //todo: make more general
+    api.verbose = true;
+    api.dataTableAttach('ExternalApis'); //todo: make more general
+
     require('../lib/account-verify')(accnum); // run account number through core account verify function
     console.log(state.vars.client_district);
     if(state.vars.client_district === 'RRT P-Shops'){
-        var client = api.getClient(account_number);
+        var client = api.getClient(accnum);
         state.vars.client_json = JSON.stringify(client);
         state.vars.TotalCredit = client.BalanceHistory[0].TotalCredit;
         state.vars.TotalRepay_Incl = client.BalanceHistory[0].TotalRepayment_IncludingOverpayments;
