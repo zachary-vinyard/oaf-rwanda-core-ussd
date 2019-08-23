d4120f9f36621c9605c0f550411144882446d95c
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2019-08-23 12:10:35 +0200

    0.0.27 goff's pocket gopher

--- pshops_lib/check_account_no.js
+++ pshops_lib/check_account_no.js
@@ -8,6 +8,11 @@ module.exports = function(accnum){
     require('../lib/account-verify')(accnum); // run account number through core account verify function
     console.log(state.vars.client_district);
     if(state.vars.client_district === 'RRT P-Shops'){
+        var client = api.getClient(account_number);
+        state.vars.client_json = JSON.stringify(client);
+        state.vars.TotalCredit = client.BalanceHistory[0].TotalCredit;
+        state.vars.TotalRepay_Incl = client.BalanceHistory[0].TotalRepayment_IncludingOverpayments;
+        state.vars.Balance = client.BalanceHistory[0].Balance;
         return true;
     }
     else{
