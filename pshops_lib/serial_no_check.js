c06ebd9cefd2b52b07864d77352233217faa3e4c
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2019-09-01 17:25:48 +0200

    0.0.37 mexican grizzly bear

--- pshops_lib/serial_no_check.js
+++ pshops_lib/serial_no_check.js
@@ -11,53 +11,47 @@ module.exports = function(accnum, serial_no){
 
     // save as variable the row from the serial table where the entered serial number matches
     var ListRows = SerialTable.queryRows({
-        vars: {'serialnumber': serial_no}
+        vars: {'serialnumber': serial_no, 'registered_account_number' : {exists : 0}}
     });
 
     // check registration status
     if(ListRows.count() === 1){
         var Serial = ListRows.next(); // this accesses the data row that you get from API cursor using queryRows
-        if(Serial.vars.accountnumber !== undefined || Serial.vars.accountnumber !== null){
-            state.vars.SerialStatus = 'AlreadyReg';
-            return null;
+        // assign account to serial number
+        state.vars.SerialStatus = 'Reg';
+        Serial.vars.accountnumber = accnum; 
+        Serial.vars.historic_credit = state.vars.TotalCredit - state.vars.Balance;
+        Serial.vars.dateregistered = moment().format("DD-MM-YYYY, HH:MM:SS");
+        Serial.save(); 
+        
+        // assign activation code
+        var ActTable = project.getOrCreateDataTable("ActivationCodes");
+        
+        // find activation code associated with input serial number
+        ListAct = ActTable.queryRows({
+            vars: {'serialnumber': serial_no,
+                    'type': "Activation",
+                    'activated': "No"
+            }
+        });
+
+        // if there's one activation code available, save the code as a state var and update the table; otherwise flag errors in source data
+        if(ListAct.count() === 1){
+            var Act = ListAct.next();
+            state.vars.ActCode = Act.vars.code;
+            Act.vars.activated = "Yes";
+            Act.vars.dateactivated = moment().format("DD-MM-YYYY, HH:MM:SS");
+            Act.save();
+        }
+        else if(ListAct.count() > 1){
+            admin_alert('duplicate rows in ActivationCodes for serial number: ' + serial_no, 'Duplicate Serial Numbers in ActTable', 'marisa');
+            state.vars.SerialStatus = 'Error';
+            return false; 
         }
         else{
-            // assign account to serial number
-            state.vars.SerialStatus = 'Reg';
-            Serial.vars.accountnumber = accnum; 
-            Serial.vars.historic_credit = state.vars.TotalCredit - state.vars.Balance;
-            Serial.vars.dateregistered = moment().format("DD-MM-YYYY, HH:MM:SS");
-            Serial.save(); 
-            
-            // assign activation code
-            var ActTable = project.getOrCreateDataTable("ActivationCodes");
-            
-            // find activation code associated with input serial number
-            ListAct = ActTable.queryRows({
-                vars: {'serialnumber': serial_no,
-                        'type': "Activation",
-                        'activated': "No"
-                }
-            });
-
-            // if there's one activation code available, save the code as a state var and update the table; otherwise flag errors in source data
-            if(ListAct.count() === 1){
-                var Act = ListAct.next();
-                state.vars.ActCode = Act.vars.code;
-                Act.vars.activated = "Yes";
-                Act.vars.dateactivated = moment().format("DD-MM-YYYY, HH:MM:SS");
-                Act.save();
-            }
-            else if(ListAct.count() > 1){
-                admin_alert('duplicate rows in ActivationCodes for serial number: ' + serial_no, 'Duplicate Serial Numbers in ActTable', 'marisa');
-                state.vars.SerialStatus = 'Error';
-                return false; 
-            }
-            else{
-                admin_alert('No rows in ActivationCodes for serial number: ' + serial_no, 'No corresponding rows in ActTable', 'marisa');
-                state.vars.SerialStatus = 'Error';
-                return false; 
-            }
+            admin_alert('No rows in ActivationCodes for serial number: ' + serial_no, 'No corresponding rows in ActTable', 'marisa');
+            state.vars.SerialStatus = 'Error';
+            return false; 
         }
     }
     else if(ListRows.count() > 1){
