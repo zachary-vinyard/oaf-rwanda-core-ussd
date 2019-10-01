9a44c36bffed224ca5c0925c5f30c6ed8c9c4da2
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2019-10-01 07:34:06 +0200

    0.0.66 a hard day's night

--- core.js
+++ core.js
@@ -35,17 +35,13 @@ addInputHandler('account_number_splash', function(input){ //acount_number_splash
         var verify = require('./lib/account-verify')
         var client_verified = verify(response);
         if(client_verified){
-            //sayText(msgs('account_number_verified'));
+            sayText(msgs('account_number_verified'));
             state.vars.account_number = response;
-            /* HIDING THIS FOR TESTING PURPOSES
             var splash = core_splash_map.queryRows({'vars' : {'district' : state.vars.client_district}}).next().vars.splash_menu;
             if(splash === null || splash === undefined){
                 admin_alert(state.vars.client_district + ' not found in district database');
                 throw 'ERROR : DISTRICT NOT FOUND';
             }
-            state.vars.splash = splash;
-            */
-            var splash = 'payg_splash_menu';
             state.vars.splash = splash;
             var menu = populate_menu(splash, lang);
             state.vars.current_menu_str = menu;
