733d822b530f11115c1e88818fa933e95fe775eb
Zachary Vinyard <30409881+zachary-vinyard@users.noreply.github.com>
2019-10-07 22:51:36 +0200

    update alert length to 10

--- traffic-alert.js
+++ traffic-alert.js
@@ -9,9 +9,9 @@ global.main = function(){
     var now = moment().format('X');
     dif_min = (now - Row.time_updated)/60;
     console.log("Last message reveived in " + dif_min +" minutes");
-    if (dif_min > 15){
-        var subject = "ALERT: No traffic on USSD service the last 15 min";
-        var body = "No traffic on USSD service the last 15 min please check https://telerivet.com/p/8799a79f/messages for more information";
+    if (dif_min > 10){
+        var subject = "ALERT: No traffic on USSD service the last 10 min";
+        var body = "No traffic on USSD service the last 10 min please check https://telerivet.com/p/8799a79f/messages for more information";
         var admin_alert = require('./lib/admin-alert');
         var alertees = ['default', 'norbert', 'tom', 'marisa', 'africas_talking']; //add alertees here. alertees must be in the 'admin_email' table in TR
         for(var i = 0; i < alertees.length; i++){
