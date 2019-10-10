fc890309c68f687929aad2df57474f1e2f0362bc
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2019-10-10 10:41:21 +0200

    0.0.2 confucius

--- auto-finalize.js
+++ auto-finalize.js
@@ -1,30 +1,27 @@
 /*
-    Script: auto-confirm.js
-    Description: confirms client USSD orders if they haven't been changed in a given timeframe
+    Script: auto-finalize.js
+    Description: labels client USSD orders as finalized if they haven't been changed in a given timeframe
     Status: in progress
 */
 
-// schedule recurring check at what frequency?
+// schedule recurring check at what frequency? when?
 
 // load necessary tables and modules
 var client_table = project.getOrCreateDataTable('enr_client_pool');
 var clients_updated = []; // potentially save a list of updated clients?
 const last_order = 14; // number of days 
+const day = 24 * 60 * 60 * 1000; // milliseconds in one day, used for date subtraction
 
-/* filter for relevant clients:
-    - not finalized
-    - order placed
-    - what's the structure of the client_table?
-*/
+// filter client table for unfinalized clients who have ordered
 var cursor = client_table.queryRows({'vars' : 
-            {   'finalized' : notfinalized,
-                'order' : notempty}
+            {   'finalized'     : 0,
+                'placed_order'  : 1}
 });
 
 // update finalized to 1
 while(cursor.hasNext()){
     client = cursor.next();
-    days_since_last_update = new Date() - client.vars.time_updated; // need to convert to days subtraction
+    days_since_last_update = Math.round(Math.abs((new Date() - client.vars.time_updated) / day)); 
     if(days_since_last_update > last_order){
         client.vars.finalized = 1;
         client.save();
