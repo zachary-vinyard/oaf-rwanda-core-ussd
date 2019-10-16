38459f1df16d7e7e70e448fcfe55c4aba014b746
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2019-10-16 12:11:02 +0200

    0.0.4 mozi

--- auto-finalize.js
+++ auto-finalize.js
@@ -6,14 +6,15 @@
 
 // load necessary tables and modules
 var client_table = project.getOrCreateDataTable('20b_client_data');
-var clients_updated = []; // potentially save a list of updated clients?
-const last_order = 7; // number of days 
+var enr_table = project.getOrCreateDataTable('Enrollment - Core + RGO');
+const last_order = 7; // number of days before an order gets finalized; can store in telerivet
 const day = 24 * 60 * 60 * 1000; // milliseconds in one day, used for date subtraction
 
 // filter client table for unfinalized clients who have ordered
 var cursor = client_table.queryRows({'vars' : 
             {   'finalized'     : 0,
-                'placed_order'  : 1}
+                'placed_order'  : 1
+            }
 });
 
 // update finalized to 1 and alert client
@@ -23,14 +24,13 @@ while(cursor.hasNext()){
     if(days_since_last_update > last_order){
         client.vars.finalized = 1;
         client.save();
-        // retrieve the phone number associated with the order
-        
-        // outgoing text to client: your order has been finalized, call the hotline if you need assistance
+        // send outgoing USSD message to client
+        client_phone = enr_table.queryRows({'vars' : {'enr_order_start' : client.vars.account_number}}).next().from_number;
         project.sendMessage({
             message_type : 'ussd',
-            content : 'The TUBURA order entered for account $ACCOUNT has been marked as final. Please call 2580 if you need assistance.', // Translate, etc
+            content : 'The TUBURA order entered for account $ACCOUNT has been marked as final. Please call 2580 if you need assistance.', // Translate, store, etc
             vars : {'$ACCOUNT' : client.vars.account_number},
-            to_number : '??', // need to retrieve the user's phone -- if in Roster?
+            to_number : client_phone
         });
         return;
     }
