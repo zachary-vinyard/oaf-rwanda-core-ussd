717b5b84fdf554abd9505c754acb13dd270fcceb
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2020-01-09 15:06:33 +0200

    0.0.20 console messages

--- extension-survey.js
+++ extension-survey.js
@@ -238,8 +238,8 @@ addInputHandler('sedo_enter_farmers', function(input){
 // input handler for start of survey
 addInputHandler('sedo_survey', function(input){
     var get_menu_option = require('./lib/get-menu-option');
-    input = input.replace(/\s/g,'');
     var crop = get_menu_option(input);
+    console.log('Input is ' + input + ' and crop is ' + crop);
     state.vars.question_number = 1;
 
     // assign the question id variable
