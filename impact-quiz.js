222767c14a8006ac10c76c2dc6e2a92ca7efc8ec
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2020-03-08 14:55:05 +0200

    0.0.3 impact quiz first draft

--- impact-quiz.js
+++ impact-quiz.js
@@ -28,8 +28,8 @@ global.main = function(){
 
 // input handler for demographic questions
 addInputHandler('demo_question', function(input){
+    // ADD HERE: steps to verify input format - text only? matches something in DB? meets max/min requirements
     // save status and response in session data
-    // ADD HERE: steps to verify input format 
     call.vars.status = state.vars.survey_type + state.vars.step;
     call.vars[call.vars.status] = input;
 
@@ -38,34 +38,35 @@ addInputHandler('demo_question', function(input){
     var survey_table = project.getOrCreateDataTable('ag_survey_questions');
     state.vars.step += 1;
 
+    // if there are remaining questions, ask the next one; else ask the first quiz question
     if(state.vars.step < survey_length){
         var question_cursor = survey_table.queryRows({'vars' : {'question_id' : state.vars.survey_type + state.vars.step}});
         var question = question_cursor.next();
         sayText(msgs(question.vars.question_text, {}, lang));
-        promptDigits('crop_demo_question', {'submitOnHash' : false, 
+        promptDigits('demo_question', {'submitOnHash' : false, 
                                             'maxDigits'    : project.vars.max_digits_for_input,
                                             'timeout'      : timeout_length});
     }
-    // execute following steps until no more demo questions, then move to survey questions
-
-    /*
-        verify input is correctly formatted:
-        - text only (needs to match something in database?) << advise Leana to go with an ID?
-        - meets max/min requirements
-
-        save input in session data
-
-        increment step
-    */
+    else{
+        // initialize variables for tracking place in impact quiz
+        state.vars.survey_type = 'trn';
+        state.vars.step = 1;
+        // ask first quiz question
+        var question = survey_table.queryRows({'vars' : {'question_id' : state.vars.survey_type + state.vars.step}}).next();
+        sayText(msgs(question.vars.question_text, {}, lang));
+        promptDigits('quiz_question', {'submitOnHash' : false, 
+                                            'maxDigits'    : project.vars.max_digits_for_input,
+                                            'timeout'      : timeout_length});
+    }
 });
 
 // input handler for survey questions
-addInputHandler('survey_question', function(input){
+addInputHandler('quiz_question', function(input){
     // test and store input response
     input = parseInt(input.replace(/\s/g,''));
     call.vars.status = state.vars.survey_type + state.vars.step;
     call.vars[call.vars.status] = input;
-    var survey_length = 8; // try to pull this directly from table
+    var survey_length = 8; // pull direct from table
 
     // verify response and retrieve relevant feedback string
     var verify = require('./lib/imp-answer-verify');
@@ -76,9 +77,11 @@ addInputHandler('survey_question', function(input){
     if(state.vars.step < survey_length){
         var ask = require('./lib/imp-ask-question');
         ask(feedback);
+        return null;
     }
     else{
         sayText(msgs('closing_message', {   '$FEEDBACK'    : feedback,
                                             '$NUM_CORRECT' : state.vars.num_correct}, lang));
+        return null;
     }
 });
