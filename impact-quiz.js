1095cb4d6636d160c3d07a865031ec8dbdbd3ea6
Marisa Bhargava <marisa.bhargava@oneacrefund.org>
2020-03-12 09:27:31 +0200

    0.0.19 added reinitization

--- impact-quiz.js
+++ impact-quiz.js
@@ -8,9 +8,7 @@
 var geo_select = require('./lib/cta-geo-select');
 var geo_process = require('./lib/cta-geo-string-processer');
 var geo_data = require('./dat/rwanda-gov-geography');
-var admin_alert = require('./lib/admin-alert');
-var reinit = require('./lib/ext-reinitization');
-var checkstop = require('./lib/ext-check-stop');
+var reinit = require('./lib/imp-reinitization');
 var msgs = require('./lib/msg-retrieve');
 
 // initialize constants
@@ -19,6 +17,7 @@ const lang = 'ki';
 // display welcome message and prompt user to run through list of demographic questions
 global.main = function(){
     // display welcome message and first geo question
+    reinit();
     var geo_list = geo_process(geo_data);
     state.vars.current_menu = JSON.stringify(geo_list);
     sayText(msgs('imp_main_splash', geo_list));
@@ -27,14 +26,14 @@ global.main = function(){
 
 // input handler for region selection
 addInputHandler('geo_selection_1', function(input){
-    state.vars.current_step = 'geo_selection_1'
+    state.vars.current_step = 'geo_selection_1';
     input = parseInt(input.replace(/\D/g,''));//cleans out anything nonnumeric in the input - really, input should only be digits 1 -?
     var keys = Object.keys(geo_data);
     if(input > 0 && input <= keys.length){
-        call.vars.region = input;
         var selection = input - 1;
         state.vars.province = selection;
         state.vars.province_name = keys[selection];
+        call.vars.province = state.vars.province_name;
         geo_data = geo_select(selection, geo_data)
         var selection_menu = geo_process(geo_data);
         state.vars.current_menu = JSON.stringify(selection_menu);
@@ -53,21 +52,24 @@ addInputHandler('geo_selection_1', function(input){
 });
 
 // input handler for district selection
-addInputHandler('geo_selection_2', function(input){
+addInputHandler('district', function(input){
     state.vars.current_step = 'geo_selection_2';
     input = parseInt(input.replace(/\D/g,''));//cleans out anything nonnumeric in the input - really, input should only be digits 1 -?
     var province = parseInt(state.vars.province);
     geo_data = geo_select(province, geo_data);
     var keys = Object.keys(geo_data);
     if(input > 0 && input <= keys.length){
-        call.vars.district = input;
-                // initialize variables for tracking place in impact quiz
-                state.vars.survey_type = 'trn';
-                state.vars.step = 1;
-                state.vars.num_correct = 0;
-                // ask first quiz question
-                var ask = require('./lib/imp-ask-question');
-                ask();
+        var selection = input - 1;
+        state.vars.district = selection;
+        state.vars.district_name = keys[selection];
+        call.vars.district = state.vars.district_name;
+        // initialize variables for tracking place in impact quiz
+        state.vars.survey_type = 'trn';
+        state.vars.step = 1;
+        state.vars.num_correct = 0;
+        // ask first quiz question
+        var ask = require('./lib/imp-ask-question');
+        ask();
     }
     else if (input == 99){ // exit
         sayText(msgs('exit')); // need to add this to the list
