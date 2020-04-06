5b99a53a7e5b3d425d330637710ce58ddfa02a0d
shemalbin <59839561+shemalbin@users.noreply.github.com>
2020-04-06 12:14:31 +0200

    connecting to telerivet

---
+++ training-survey.js
@@ -0,0 +1,15 @@
+
+var msgs = require('./lib/msg-retrieve');
+
+const max_digits_for_account_number = project.vars.max_digits_an;
+
+global.main = function () {
+    sayText(msgs('cor_main_splash'));
+    promptDigits('account_number_splash', { 'submitOnHash' : false,
+                                            'maxDigits'    : max_digits_for_account_number,
+                                            'timeout'      : 180 });
+};
+
+addInputHandler('ext_main_splash', function(input){
+    sayText(input);
+});
