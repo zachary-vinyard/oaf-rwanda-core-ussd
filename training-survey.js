5ccf3bceb0950cfa4ab568c67deb8644a9b2f79d
Sabin Sheja <sabin.sheja@oneacrefund.org>
2020-04-06 17:25:51 +0200

    connecting to telerivet

--- training-survey.js
+++ training-survey.js
@@ -1,15 +1,21 @@
-
+var geo_select = require('./lib/cta-geo-select');
+var geo_process = require('./lib/cta-geo-string-processer');
+var geo_data = require('./dat/rwanda-tubura-geography');
+var msgs = require('./lib/msg-retrieve');
 var msgs = require('./lib/msg-retrieve');
 
 const max_digits_for_account_number = project.vars.max_digits_an;
 
 global.main = function () {
-    sayText(msgs('cor_main_splash'));
-    promptDigits('account_number_splash', { 'submitOnHash' : false,
+
+    var geo_list = geo_process(geo_data);
+    state.vars.current_menu = JSON.stringify(geo_list);
+    sayText(msgs('train_main_splash', geo_list));
+    promptDigits('district', { 'submitOnHash' : false,
                                             'maxDigits'    : max_digits_for_account_number,
                                             'timeout'      : 180 });
 };
 
-addInputHandler('account_number_splash', function(input){
-    sayText('you have entered'+input);
+addInputHandler('district', function(input){
+    sayText('you have entered'+ input);
 });
